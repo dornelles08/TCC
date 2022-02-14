dadb67e8091fa3646e7c607f8adc9aa7a77fee90
Felipe Dornelles <lipe.dornelles@hotmail.com>
2022-02-14 16:05:12 -0300

    Documentação do projeto

--- Extração de Dados Da OLX/main.py
+++ Extração de Dados Da OLX/main.py
@@ -2,13 +2,16 @@ import subprocess
 from time import time
 
 inicio = time()
-scripts = ["MongoToCsv.js", "CsvToPostgres.js", "ConvertStringToCode.js", "TransdormandoDados.js"]
+scripts = ["MongoToCsv.js",
+           "CsvToPostgres.js",
+           "ConvertStringToCode.js",
+           "TransdormandoDados.js"]
 
 subprocess.run(["npm", "install"])
 
 for script in scripts:
-  subprocess.run(["clear"])
-  subprocess.run(["node", script])
+    subprocess.run(["clear"])
+    subprocess.run(["node", script])
 
 fim = time()
 
